637fe64c5f396b650c668569d7a891bb80941d76
MPower26 <121577980+MPower26@users.noreply.github.com>
2025-04-24 02:06:08 +0100

    retry deploy

--- new_backend/startup.sh
+++ new_backend/startup.sh
@@ -1,2 +1,15 @@
-#!/bin/bash
-gunicorn app.main:app --config gunicorn.conf.py 
+
+# Set environment variables
+export PYTHONUNBUFFERED=1
+export WEBSITE_HOSTNAME=localhost:8000
+export PORT=8000
+
+# Change to the application directory
+cd /home/site/wwwroot
+
+# Install dependencies
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+# Start the application
+gunicorn app.main:app --config gunicorn.conf.py
