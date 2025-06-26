53e3862bec702ca7c99dcdd36e98674fe75e2f07
thomaswmellor-eng <tom@wesiagency.com>
2025-06-26 20:16:28 +0200

    Update gunicorn.conf.py

--- new_backend/gunicorn.conf.py
+++ new_backend/gunicorn.conf.py
@@ -7,13 +7,13 @@ bind = f"0.0.0.0:{os.getenv('PORT', '8000')}"
 backlog = 2048
 
 # Worker processes
-workers = 1
+workers = 2
 worker_class = 'uvicorn.workers.UvicornWorker'
 worker_connections = 1000
-timeout = 2000
-keepalive = 2
+timeout = 1800  # 30 minutes
+keepalive = 5
 max_requests = 1000
-max_requests_jitter = 50
+max_requests_jitter = 100
 
 # Logging
 accesslog = '-'
