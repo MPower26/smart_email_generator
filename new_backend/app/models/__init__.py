826bfa00f33af2dcfde648989fe41368bb3535f7
thomaswmellor-eng <tom@wesiagency.com>
2025-07-13 20:39:22 +0200

    Update __init__.py

--- new_backend/app/models/__init__.py
+++ new_backend/app/models/__init__.py
@@ -1,1 +1,10 @@
 # This file makes the models directory a Python package 
+
+from .models import (
+    User, VerificationCode, EmailTemplate, GeneratedEmail, 
+    EmailGenerationProgress, Attachment, SentHistory, FriendRequest
+)
+from .anti_spam_models import (
+    EmailDailyLimit, SenderReputation, AuthorizedDomain, 
+    EmailSendLog, EmailLimitRule
+) 
