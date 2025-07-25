42931fe828480be7a7f34905d01df0413ba4292f
thomaswmellor-eng <tom@wesiagency.com>
2025-07-25 23:58:29 +0200

    lk

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Navigation from './components/Navigation';
 import WaitlistPage from './pages/WaitlistPage';
 import './App.css';
 import Footer from './components/Footer';
 
+const AppContent = () => {
+  const location = useLocation();
+  const isWaitlistPage = location.pathname === '/';
+
+  return (
+    <div className={`App ${isWaitlistPage ? 'waitlist-page' : ''}`}>
+      <Routes>
+        <Route path="/" element={<WaitlistPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+};
+
 function App() {
   return (
     <Router>
-      <div className="App d-flex flex-column min-vh-100">
-        <Routes>
-          <Route path="/" element={<WaitlistPage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-        <Footer />
-      </div>
+      <AppContent />
     </Router>
   );
 }
